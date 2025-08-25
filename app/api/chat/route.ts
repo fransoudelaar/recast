059ee62a2e706a2fbfd1b10a112b02b702ca2cd3
Frans Oudelaar <>
2025-08-25 10:51:56 +0200

    feat: add weather tool

--- app/api/chat/route.ts
+++ app/api/chat/route.ts
@@ -1,6 +1,7 @@
 // import { openai } from "@ai-sdk/openai";
 import { google } from "@ai-sdk/google";
-import { streamText } from "ai";
+import { streamText, tool } from "ai";
+import z from "zod";
 
 // Allow streaming responses up to 30 seconds
 export const maxDuration = 30;
@@ -27,6 +28,35 @@ export async function POST(req: Request) {
   const result = streamText({
     model: google("gemini-2.5-pro"), // "gemini-2.0-flash-001"
     messages,
+    tools: {
+      weather: tool({
+        description: "Get the weather in a location (fahrenheit)",
+        parameters: z.object({
+          location: z.string().describe("The location to get the weather for"),
+        }),
+        execute: async ({ location }) => {
+          const temperature = Math.round(Math.random() * (90 - 32) + 32);
+          return {
+            location,
+            temperature,
+          };
+        },
+      }),
+      convertFahrenheitToCelsius: tool({
+        description: "Convert a temperature in fahrenheit to celsius",
+        parameters: z.object({
+          temperature: z
+            .number()
+            .describe("The temperature in fahrenheit to convert"),
+        }),
+        execute: async ({ temperature }) => {
+          const celsius = Math.round((temperature - 32) * (5 / 9));
+          return {
+            celsius,
+          };
+        },
+      }),
+    },
   });
 
   return result.toDataStreamResponse({
