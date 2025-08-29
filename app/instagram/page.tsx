498e0bebd6783c3ae05ab3c618cdfcc23388de36
Frans Oudelaar <>
2025-08-29 08:23:30 +0200

    feature: change name

--- app/instagram/page.tsx
+++ app/instagram/page.tsx
@@ -156,9 +156,14 @@ export default function InstaPostGenerator() {
             )}
           </>
         ) : (
-          <h1 className="text-2xl font-semibold leading-normal bg-gradient-to-r from-blue-500 to-purple-600 inline-block text-transparent bg-clip-text">
-            Insta Post Generator®
-          </h1>
+          <div>
+            <h1 className="text-2xl font-semibold leading-normal bg-gradient-to-r from-blue-500 to-purple-600 inline-block text-transparent bg-clip-text">
+              ReCast®{' '}
+            </h1>
+            <span className="text-sm bg-gradient-to-r from-neutral-400 to-neutral-500 ml-4 text-transparent bg-clip-text">
+              Cast your rig. ReCast your content.
+            </span>
+          </div>
         )}
       </div>
     </div>
